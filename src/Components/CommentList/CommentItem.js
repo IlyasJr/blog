c59a17b9a30db5bfd9a86d540ada40dc53f93c32
Ilyas hamdi <ily.hamdi@gmail.com>
2020-12-11 10:04:40 -0500

    chore:comment component

---
+++ src/Components/CommentList/CommentItem.js
@@ -0,0 +1,10 @@
+
+
+export const CommentItem = () => {
+  return (
+    <div className= 'commentitem'>
+      <img src="src/logo.svg" alt="this an avatar" />
+      <p> this is a comment </p>
+    </div>
+  );
+};
