01cfc59767960bc52c5f66cbff9bd3e8452a67e1
Ilyas hamdi <ily.hamdi@gmail.com>
2020-12-11 10:47:09 -0500

    chore : le voila

---
+++ src/Components/Search/index.js
@@ -0,0 +1,8 @@
+export const Search = () => {
+    return (
+        <div className = 'search-container'>
+         <input type ='text' placeholder ='type here to search'/>
+         <button>Search</button>  
+        </div>
+    )
+}
---
+++ src/Components/Topbar/Topbar.js
@@ -0,0 +1,15 @@
+import { Search } from "../Search";
+
+export const Topbar = () => {
+  return (
+    <div className="topbar">
+      <img src="si" alt="this is a topbar" />
+      <Search />
+      <div className='buttons-container'>
+        <button>logout</button>
+        <button>login</button>
+        <button>register</button>
+      </div>
+    </div>
+  );
+};
